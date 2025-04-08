da43eee5b5d816c1bdd4195bb59cbf1601ec57bf
Precious Victor <preciousv308gmail.com>
2025-04-08 13:18:53 +0100

    Updated site content

--- src/components/Blakefoot.js
+++ src/components/Blakefoot.js
@@ -1,41 +1,40 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react'
-import Image from '../assets/images/apple.svg'
-import Image1 from '../assets/images/playstore.svg'
-import '../styles/Blake.css'
+import React from 'react';
+import '../styles/Blake.css';
+import playstore from '../assets/images/playstore.svg';
+import apple from '../assets/images/apple.svg';
 
-function Blakefoot() {
+function Footer() {
   return (
-    <div className='footer'>
-      <div className='footerDiv'>
-
+    <footer className="footer section-p1">
+      <div className="footerDiv">
         <div className="footer1">
           <h3>CONTACT US</h3>
-          <p>Phone: +2349039615666</p>
-          <p>Email:<a href=""> hello@myblake.app</a></p>
+          <p>
+            Phone: <span style={{ color: 'black' }}>+2349039615666</span>
+          </p>
+          <p>Email: hello@myblake.app</p>
         </div>
-
         <div className="footer1">
           <h3>ADDRESS</h3>
-          <p>Elzazi complex, Opposite Westharm petrol station along gbalajam/Akpajo road, woji ( Odili Road,
-            Port-Harcourt )</p>
+          <p>
+            Elzazi complex, Opposite Westharm petrol station along gbalajam/Akpajo road, woji (Odili Road, Port-Harcourt)
+          </p>
         </div>
-
         <div className="footer1">
           <h3>DOWNLOAD</h3>
-          <div className='footerImg'>
-            <div className='imgs'>
-              <img src={Image} alt='' />
+          <div className="footerImg">
+            <div className="imgs">
+              <img src={playstore} alt="Playstore" />
             </div>
-            <div className='imgs'>
-              <img src={Image1} alt='' />
+            <div className="imgs">
+              <img src={apple} alt="Apple Store" />
             </div>
           </div>
         </div>
-
       </div>
-    </div>
-  )
+    </footer>
+  );
 }
 
-export default Blakefoot
+export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,63 @@
-import React from 'react'
-import '../styles/Blake.css'
+import React from 'react';
+import '../styles/Blake.css';
 
 function Header() {
+
+  const container = document.querySelector('.header-container');
+
+  window.addEventListener('scroll', () => {
+    const scrollPosition = window.scrollY;
+    if (scrollPosition > 20) {
+      container.style.backgroundColor = '#ffffff';
+      container.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.1)';
+    } else {
+      container.style.backgroundColor = '#fff3e4';
+      container.style.boxShadow = 'none';
+    }
+  })
+
+  const navToggle = document.getElementById('navToggle')
+  const navOverlay = document.getElementById('navOverlay')
+  const rightContainer = document.querySelector('.right-container')
+
+  navToggle.addEventListener('click', () => {
+    if (navToggle) {
+      navOverlay.classList.add('show')
+      rightContainer.classList.add('show')
+    }
+  })
+
+  navOverlay.addEventListener('click', () => {
+    if (navOverlay) {
+      navOverlay.classList.remove('show')
+      rightContainer.classList.remove('show')
+    }
+  })
   return (
     <div>
-      <section className="header-container scrolled">
-        <div className="left-container">
-          <div className='title'>
-            <div className='logo'></div>
-            <p>Build with Blake</p>
+      <header className="header-container">
+        <div className="header">
+          <div className="left-container">
+            <div className="logoHead">
+              <div className="logo"></div>
+              <p>Build with Blake</p>
+            </div>
           </div>
-        </div>
 
-        <div className="right-container">
-          <button className="call">Let's Call You</button>
-          <button className="build">Let's Build For You</button>
+          <div className="right-container">
+            <div className="logoHead logoHeadNav">
+              <div className="logo"></div>
+              <p>Build with Blake</p>
+            </div>
+            <button className="call">Let's Call You</button>
+            <button className="build">Let's Build For You</button>
+          </div>
+          <div id="navToggle" style={{ fontSize: '40px' }}>&times;</div>
         </div>
-      </section>
+        <div id="navOverlay"></div>
+      </header>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
